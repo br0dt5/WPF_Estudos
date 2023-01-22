dbe0451d545c59e9c9d959c07f65d5a87cc1b994
Luan Zaniqueli <luanzaniqueli.carlos@yahoo.com.br>
2023-01-22 14:09:43 -0300

    Primeiros projetos adicionados.

---
+++ ChatApp/ChatApp/Net/IO/PacketReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net.IO
+{
+    class PacketReader : BinaryReader
+    {
+        private NetworkStream _ns;
+        public PacketReader(NetworkStream ns) : base(ns)
+        {
+            _ns = ns;
+        }
+
+        public string ReadMessage()
+        {
+            byte[] msgbuffer;
+            var length = ReadInt32();
+            msgbuffer = new byte[length];
+            _ns.Read(msgbuffer, 0, length);
+
+            var msg = Encoding.ASCII.GetString(msgbuffer);
+            return msg;
+        }
+    }
+}
